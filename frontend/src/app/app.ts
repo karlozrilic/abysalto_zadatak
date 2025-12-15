ca8f3fe8a1440b7bad49bb3e55e4f766dabeb71e
karlozrilic <karlozrilic@gmail.com>
2025-12-15 10:19:02 +0100

    Added prime ng to project

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,5 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
 import { BackendService } from './services/backend.service';
 
 interface Product {
@@ -12,7 +13,10 @@ interface Product {
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    ButtonModule
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
